8d30dd1091dbcf3b494cb207f78feffce22f1af4
Darco2903 <william.valenduc@orange.fr>
2025-12-24 18:34:24 +0100

    socket.io remove

--- src/socket/types.ts
+++ src/socket/types.ts
@@ -1,10 +1,7 @@
 import type { Socket as ClientSocket } from "socket.io-client";
-import type { Socket as ServerSocket } from "socket.io";
 import {
     CdnClientToServerEvents as ClientToServer,
     CdnServerToClientEvents as ServerToClient,
 } from "./interface/index.js";
 
 export type CdnClientSocket = ClientSocket<ServerToClient, ClientToServer>;
-
-export type CdnServerSocket = ServerSocket<ClientToServer, ServerToClient>;
